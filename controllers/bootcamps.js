5d09db64698f2be29e15928df3343e2c1e079dc9
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2021-12-06 16:32:13 -0300

    04x06 - Fetching Bootcamps - GET

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,17 +3,38 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-    res
-        .status(200)
-        .json({ success: true, msg: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+
+        res.status(200).json({
+            success: true,
+            count: bootcamps.length,
+            data: bootcamps
+        })
+    } catch (err) {
+        res.status(400).json({ success: false, error: err });
+    }
 }
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        if (!bootcamp) {
+            return res.status(404).json({ success: false, error: `Bootcamp not found` });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: bootcamp
+        });
+    } catch (err) {
+        res.status(400).json({ success: false, error: err });
+    }
 }
 
 // @desc    Create new bootcamp
